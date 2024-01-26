1d2c6b9760458cf0e93c44dd5184887a18f17d6a
lavanya-shende <220120022@iitdh.ac.in>
2024-01-26 20:01:00 +0530

    page4 done

--- script.js
+++ script.js
@@ -2,3 +2,36 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 })
+
+var a = document.querySelector('#element-container')
+var fixed = document.querySelector("#fixed-image")
+a.addEventListener("mouseenter",function(){
+    fixed.style.display = "block"
+})
+
+a.addEventListener("mouseleave",function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll("#element")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image=e.getAttribute("data-img")
+        fixed.style.backgroundImage=`url(${image})`
+    })
+})
+
+var headings = document.querySelectorAll('h3')
+var b=document.querySelector("#div2")
+
+headings.forEach(function(h){
+    h.addEventListener("click",function(){
+        headings.forEach(function (otherH) {
+            otherH.style.color = "grey";
+        });
+
+        h.style.color="white"
+        var image=h.getAttribute("data-img")
+        b.style.backgroundImage=`url(${image})`
+    })
+})
